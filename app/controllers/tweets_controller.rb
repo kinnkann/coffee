df53af41ca1b29d42eb3b46b5ffce7b60565bbb5
kinnkann <daiki.takashima@di-v.co.jp>
2020-02-23 17:47:04 +0900

    初
    じゃあ

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,22 @@
 class TweetsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
 end
